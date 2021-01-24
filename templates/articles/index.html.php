72e7316740ec4301abbceca4d6c44e6231a87ef5
Ahoni <asfaenti.zoubert@hotmail.com>
2021-01-24 22:50:33 +0100

    placement des card 3 par lignes

--- templates/articles/index.html.php
+++ templates/articles/index.html.php
@@ -11,14 +11,14 @@
 
 <div class="row">
     <?php foreach ($articles as $article) : ?>
-        <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+        <div class="card text-white bg-primary col-3 m-3">
             <div class="card-header">Article</div>
             <div class="card-body">
                 <h2 class="card-title"><?= $article['title'] ?></h2>
                 <small>Ecrit le <?= $article['created_at'] ?></small>
                 <p class="card-text"><?= $article['introduction'] ?></p>
             </div>
-            <button type="button" class="btn btn-secondary"><a href="index.php?controller=article&task=show&id=<?= $article['id'] ?>">Lire la suite</a></button>
+            <button type="button" class="btn btn-secondary my-2"><a href="index.php?controller=article&task=show&id=<?= $article['id'] ?>">Lire la suite</a></button>
             <button type="button" class="btn btn-secondary">Secondary</button>
         </div>
     <?php endforeach ?>
