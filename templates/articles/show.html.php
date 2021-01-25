4067f909dd342e4e4277d5edb2cf20eacd77052d
Ahoni <asfaenti.zoubert@hotmail.com>
2021-01-25 02:39:25 +0100

    ajout tableau des commentaires
    ajout de la tableau des commentaires + rearrangement des elements

--- templates/articles/show.html.php
+++ templates/articles/show.html.php
@@ -11,23 +11,10 @@
 <?= $article['content'] ?>
 
 <hr>
-<?php if (count($commentaires) === 0) : ?>
-    <h2>Il n'y a pas encore de commentaires pour cet article ... SOYEZ LE PREMIER ! :D</h2>
-<?php else : ?>
-    <h2>Il y a déjà <?= count($commentaires) ?> réactions : </h2>
-    <?php foreach ($commentaires as $commentaire) : ?>
-        <h3>Commentaire de <?= $commentaire['author'] ?></h3>
-        <small>Le <?= $commentaire['created_at'] ?></small>
-        <blockquote>
-            <em><?= $commentaire['content'] ?></em>
-        </blockquote>
-        <a href="index.php?controller=comment&task=delete&id=<?= $commentaire['id'] ?>" onclick="return window.confirm(`Êtes vous sûr de vouloir supprimer ce commentaire ?!`)">Supprimer</a>
-    <?php endforeach ?>
-<?php endif ?>
-
+<!-- formulaire des commentaires  -->
 <form action="index.php?controller=comment&task=insert" method="POST">
     <fieldset>
-        <h3>Vous voulez réagir ? N'hésitez pas les bros !</h3>
+        <h4 class="bg-info">Vous voulez réagir ? N'hésitez pas les bros !</h4>
         <div class="form-group ">
 
             <input type="text" name="author" placeholder="Votre pseudo !">
@@ -41,3 +28,34 @@
         <button>Commenter !</button>
     </fieldset>
 </form>
+
+<!-- Tableau des commentaires -->
+<?php if (count($commentaires) === 0) : ?>
+    <h2>Il n'y a pas encore de commentaires pour cet article ... SOYEZ LE PREMIER ! :D</h2>
+<?php else : ?>
+    <h4>Il y a déjà <?= count($commentaires) ?> réactions : </h4>
+    <table class="table table-hover col align-self-center">
+        <thead>
+            <tr>
+                <th scope="col">Commentaire de </th>
+                <th scope="col">Ajouter le </th>
+                <th scope="col">Commentaire</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <?php foreach ($commentaires as $commentaire) : ?>
+            <tbody>
+                <tr class="table-active">
+                    <th scope="row"><?= $commentaire['author'] ?></th>
+                    <td><?= $commentaire['created_at'] ?></td>
+                    <td>
+                        <blockquote>
+                            <em><?= $commentaire['content'] ?></em>
+                        </blockquote>
+                    </td>
+                    <td><a href="index.php?controller=comment&task=delete&id=<?= $commentaire['id'] ?>" onclick="return window.confirm(`Êtes vous sûr de vouloir supprimer ce commentaire ?!`)">Supprimer</a></td>
+                </tr>
+            </tbody>
+        <?php endforeach ?>
+    </table>
+<?php endif ?>
